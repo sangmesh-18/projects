415a2c35717fed2a7ccdb5824638e839fd1250cd
sangmesh-18 <131734721+sangmesh-18@users.noreply.github.com>
2024-10-03 11:30:23 +0530

    Add files via upload

---
+++ job Portal/frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './App.css'
+import Home from './components/Home'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+
+const appRouter = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/login',
+    element:<Login/>
+  },
+  {
+    path:'/signup',
+    element:<Signup/>
+  },
+
+])
+function App() {
+ 
+
+  return (
+    <>
+     <RouterProvider router={appRouter}/>
+    </>
+  )
+}
+
+export default App

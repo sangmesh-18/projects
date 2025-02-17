98f037f742373a1aed5981bd47bc7f1515d4ec52
sangmesh-18 <131734721+sangmesh-18@users.noreply.github.com>
2025-02-17 11:36:07 +0530

    Update Navbar.jsx

--- job Portal/frontend/src/components/shared/Navbar.jsx
+++ job Portal/frontend/src/components/shared/Navbar.jsx
@@ -15,6 +15,10 @@ const Navbar = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
+    const handleRedirect = () => {
+            navigate('/')
+    };
+
     const logoutHandler = async () => {
         try {
             const res = await axios.get(`${USER_API_END_POINT}/logout`, { withCredentials: true });
@@ -31,9 +35,9 @@ const Navbar = () => {
     return (
         <div className='bg-white'>
             <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
-                <div>
-                    <h1 className='text-2xl font-bold'>Job<span className='text-[#F83002]'>Portal</span></h1>
-                </div>
+                 <Link to="/" onClick={handleRedirect}>
+                    <h1  className='text-2xl font-bold'>Job<span className='text-[#F83002]'>Portal</span></h1>
+                </Link>
                 <div className='flex items-center gap-12'>
                     <ul className='flex font-medium items-center gap-5'>
                         {

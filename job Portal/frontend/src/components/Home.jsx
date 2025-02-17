5153436719faca042a971787276714c13cb3e535
sangmesh-18 <131734721+sangmesh-18@users.noreply.github.com>
2025-02-17 11:37:45 +0530

    Update Home.jsx

--- job Portal/frontend/src/components/Home.jsx
+++ job Portal/frontend/src/components/Home.jsx
@@ -6,17 +6,21 @@ import LatestJobs from './LatestJobs'
 import Footer from './shared/Footer'
 import useGetAllJobs from '@/hooks/useGetAllJobs'
 import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import { useLocation,useNavigate } from 'react-router-dom'
+
 
 const Home = () => {
   useGetAllJobs();
   const { user } = useSelector(store => store.auth);
   const navigate = useNavigate();
+  const location = useLocation();
   useEffect(() => {
-    if (user?.role === 'recruiter') {
-      navigate('/admin/companies')
+    // Check if user exists, has a role of 'recruiter', and is NOT already on '/' or '/admin/companies'
+    if (user?.role === 'recruiter' && location.pathname !== '/' && location.pathname !== '/admin/companies') {
+      navigate('/admin/companies');
     }
-  },[])
+  }, [user, location, navigate]);
+  
   return (<div className='mx-auto'>
     <Navbar />
     <HeroSection />
